6900c8add0c62a4d9b4155bc0a987a5817997fe4
Pontus <pontus00valdes@gmail.com>
2022-06-01 08:40:23 +0200

    changed the zodiac text

--- src/components/modals/CapricornModal.js
+++ src/components/modals/CapricornModal.js
@@ -15,16 +15,25 @@ const CapricornModal = ({ closeModalCapricorn }) => {
 						<XIcon className="w-5 h-5" />
 					</div>
 					<div className="w-full text-white text-md font-sans text-justify">
-						Help Needed!
-						<br /> <br />
-						What we need for these pages is information about
-						the Zodiac Signs.
-						<br />
-						You don't have to know about every Sign. Write to
-						me and we'll have a talk about how we can help
-						each other out.
-						<br /> Please head over to the "about" section to
-						find contact information!
+						<p className="leading-6">
+							This is a project I have been working on to
+							show some of my web-dev abilities. I am a
+							passionate programmer that also is very into
+							astrology. I am also in need of some help to
+							make this website even better. So if you want
+							and have some knowledge we could help each
+							other out. We need information about the
+							Zodiac Signs. If you know, or know anybody
+							that knows about the signs please contact me
+							at{" "}
+							<span className="font-bold">
+								pontusabrahamsson92@gmail.com
+							</span>
+							, Name your Email "Astrological Calling" and
+							I will get back to you asap. If you are
+							interested in working with me, Please use the
+							same Email and Naming on the email!
+						</p>
 					</div>
 				</div>
 			</div>
